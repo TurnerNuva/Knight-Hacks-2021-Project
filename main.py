97affc0dbaf38c59e1e8e3368a27f59a98b20c76
Turner D. Flynn <turnerdflynn@gmail.com>
2021-11-14 02:07:53 -0500

    more organization

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import io
 #jetson nano facial recognition
 #===============================================================================================================
 
-
+#INSERT HERE
 
 #record audio
 #===============================================================================================================
@@ -90,9 +90,62 @@ def transcribe_file(speech_file):
 
     # Each result is for a consecutive portion of the audio. Iterate through
     # them to get the transcripts for the entire audio file.
-    for result in response.results:
-        # The first alternative is the most likely one for this portion.
-        print(u"Transcript: {}".format(result.alternatives[0].transcript))
+    
+    # for result in response.results:
+    #     # The first alternative is the most likely one for this portion.
+    #     print(u"Transcript: {}".format(result.alternatives[0].transcript))
+
+    return response
+
+#Display to oled screen
+#===============================================================================================================
+def display_message(response_string):
+    # 128x32 display with hardware I2C:
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_bus=1, gpio=1) # setting gpio to 1 is hack to avoid platform detection
+
+    # Initialize library.
+    disp.begin()
+
+    # Clear display.
+    disp.clear()
+    disp.display()
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Draw some shapes.
+    # First define some constants to allow easy resizing of shapes.
+    padding = -2
+    top = padding
+    bottom = height-padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+    x = 0
+
+    # Load default font.
+    font = ImageFont.load_default()
+
+    while True:
+        # Write two lines of text.
+        draw.text((x, top),       "test",  font=font, fill=255)
+        draw.text((x, top+8),     "test", font=font, fill=255)
+        draw.text((x, top+16),    "test",  font=font, fill=255)
+        draw.text((x, top+24),    "test",  font=font, fill=255)
+        draw.text((x, top+32),    "test",  font=font, fill=255)
+        draw.text((x, top+40),    "test",  font=font, fill=255)
+        # Display image.
+        disp.image(image)
+        disp.display()
+        time.sleep(.1)
+
 
 def main():
     
@@ -100,7 +153,9 @@ def main():
 
     audio_file = record_audio()
 
-    transcribe_file(audio_file())
+    response_string = transcribe_file(audio_file)
+
+    display_message(response_string)
 
 
 if __name__ == '__main__':
