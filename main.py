3e4e2633ac529dfec2d7cf4daa36f61d0be0e233
Turner D. Flynn <turnerdflynn@gmail.com>
2021-11-14 01:29:48 -0500

    organize the main file

---
+++ main.py
@@ -0,0 +1,107 @@
+import jetson.inference
+import jetson.utils
+
+import pyaudio
+import wave
+import pysoundfile as sf
+
+import time
+
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+from google.cloud import speech
+import io
+
+
+#jetson nano facial recognition
+#===============================================================================================================
+
+
+
+#record audio
+#===============================================================================================================
+def record_audio():
+
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 2
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 3
+    filename = "output.wav"
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print('Recording')
+
+    stream = p.open(format=sample_format, channels=channels, rate=fs, frames_per_buffer=chunk, input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    # file convert
+    data, samplerate = sf.read('output.wav')
+    sf.write('audio.flac', data, samplerate)
+
+    return "audio.flac"
+
+#Google API
+#===============================================================================================================
+
+def transcribe_file(speech_file):
+
+    client = speech.SpeechClient()
+
+    with io.open(speech_file, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100,
+        language_code="en-US",
+    )
+
+    response = client.recognize(config=config, audio=audio)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u"Transcript: {}".format(result.alternatives[0].transcript))
+
+def main():
+    
+
+
+    audio_file = record_audio()
+
+    transcribe_file(audio_file())
+
+
+if __name__ == '__main__':
+    main()
