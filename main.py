51b285470cfaba102f2095961d1d764bc1939864
Turner D. Flynn <turnerdflynn@gmail.com>
2021-11-14 06:58:19 -0500

    Better organization

--- main.py
+++ main.py
@@ -93,24 +93,25 @@ def record_audio():
 #===============================================================================================================
 
 def transcribe_file(speech_file):
-	
-	client = speech.SpeechClient()
 
-	with io.open(speech_file, "rb") as audio_file:
-		content = audio_file.read()
+    client = speech.SpeechClient()
 
-	audio = speech.RecognitionAudio(content=content)
+    with io.open(speech_file, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
     config = speech.RecognitionConfig(encoding=speech.RecognitionConfig.AudioEncoding.FLAC, sample_rate_hertz=44100, language_code="en-US")
-	response = client.recognize(config=config, audio=audio)
+    response = client.recognize(config=config, audio=audio)
 
     # Each result is for a consecutive portion of the audio. Iterate through
     # them to get the transcripts for the entire audio file.
-    
+
     for result in response.results:
     # The first alternative is the most likely one for this portion.
         print(u"Transcript: {}".format(result.alternatives[0].transcript))
 
-    return response
+        data = result.alternatives[0].transcript
+    return data
 
 #Display to oled screen
 #===============================================================================================================
